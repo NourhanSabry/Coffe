2af37b3c24964456f0fed0c42993fc36254f5b8e
Bassant Tarek <basantsallam2001@gmail.com>
2022-08-16 21:21:30 -0700

    before last commit
    we have small issues and we need your help today Ayat--- nourhan

--- CoffeeProjecttt/ViewControllers/ViewControllerProfile.swift
+++ CoffeeProjecttt/ViewControllers/ViewControllerProfile.swift
@@ -10,15 +10,19 @@ import CoreData
 
 class ViewControllerProfile: UIViewController {
 
+    @IBOutlet weak var logout: UIButton!
     @IBOutlet weak var profEmail: UILabel!
     @IBOutlet weak var profUsername: UILabel!
     @IBOutlet weak var profName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        logout.layer.cornerRadius = 25
         
-       let testt = users[users.endIndex]
+       //let testt = users[users.endIndex]
         
-      
+        var user = NSEntityDescription.entity(forEntityName: "Users",
+            in: context)!
+        var testt = Users(entity: user, insertInto:nil)
         
         profEmail.text = testt.email
         profName.text = testt.name
@@ -27,7 +31,14 @@ class ViewControllerProfile: UIViewController {
       
     }
     
-
+    @IBAction func logOut(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
+                           let loginVC = storyboard.instantiateViewController(withIdentifier: "ViewControllerLogin") as! ViewControllerLogin
+                    
+                    self.navigationController?.pushViewController(loginVC, animated: true)
+        
+    }
+    
   
 
 }
